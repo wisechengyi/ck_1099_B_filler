d94bd13d2e579b8ec1f2bb916b1a28cc58baddf4
wisechengyi <wisechengyi@gmail.com>
2019-02-17 23:11:44 -0800

    add sample data

---
+++ real_data.py
@@ -0,0 +1,5 @@
+total = '''\
+Description of property (Example 100 sh. XYZ Co.)	Date acquired	Date sold or disposed	Proceeds	Cost or other basis	Accrued market discount	Wash sale loss disallowed	Short-Term gain loss Long-term gain or loss Ordinary	Form 8949 Code	Check if basis reported to IRS	Federal income tax withheld	Check if noncovered security	Reported to IRS: Gross proceeds Net proceeds
+10.00 TWITTER INC	12/03/2016	02/12/2018	100.01	200.35	$0.00	$0.00	Long Term	E	No	0	Uncovered	Gross proceeds
+11.00 TWITTER INC	02/05/2017	02/15/2018	300.9	400.6	$0.00	$0.00	Long Term	E	No	0	Uncovered	Gross proceeds
+12.00 TWITTER INC	03/06/2017	05/05/2018	500.33	600.77	$0.00	$0.00	Long Term	E	No	0	Uncovered	Gross proceeds'''
